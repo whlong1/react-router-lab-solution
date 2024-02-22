268c2d715dbfde85763058f33150060e2e50c787
whlong1 <whunterlong@gmail.com>
2024-02-22 11:51:11 -0500

    Add nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"
+
+const Nav = () => {
+  return (
+    <nav>
+      <Link to="mailboxes">Mailboxes</Link>
+      <Link to="new-mailbox">New Mailbox</Link>
+      <Link to="new-letter">New Letter</Link>
+    </nav>
+  )
+}
+
+export default Nav
