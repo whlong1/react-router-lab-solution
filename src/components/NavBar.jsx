f2ef1d6f323916d641104d1f76888b709da5414d
whlong1 <whunterlong@gmail.com>
2024-02-23 17:18:21 -0500

    Fix formatting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/mailboxes">Mailboxes</Link>
-      <Link to="/new-mailbox">New Mailbox</Link>
-      <Link to="/new-letter">New Letter</Link>
+      <Link to='/'>Home</Link>
+      <Link to='/mailboxes'>Mailboxes</Link>
+      <Link to='/new-mailbox'>New Mailbox</Link>
+      <Link to='/new-letter'>New Letter</Link>
     </nav>
   );
 };
